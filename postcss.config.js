6056f066acbd597a382ad561c4e2eae648c6b8b9
Benoit Eccli <beneccli@gmail.com>
2020-02-28 23:06:47 +0100

    Add tailwind base into build

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,19 @@
 const isProd = process.env.NODE_ENV === 'production';
 
+const purgecss = {
+  content: 
+    [
+      './pages/**/*.{js,jsx,ts,tsx}',
+      './components/**/*.{js,jsx,ts,tsx}',
+    ],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+};
+
 module.exports = {
   plugins: {
     'postcss-import': {},
     'tailwindcss': {},
-    '@fullhuman/postcss-purgecss': isProd ? {
-      content: 
-        [
-          './pages/**/*.{js,jsx,ts,tsx}',
-          './components/**/*.{js,jsx,ts,tsx}',
-        ],
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-    } : false,
+    '@fullhuman/postcss-purgecss': isProd ? purgecss : false,
     'postcss-nested': {},
     'postcss-custom-properties': {},
     'postcss-flexbugs-fixes': {},
